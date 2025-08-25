0d0e17b99ccc695ccafe35bd3cbc025edc453360
maintainline <z.seon.dev@gmail.com>
2025-08-25 12:19:52 +0900

    [docs] useState 예제 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-type SampleProps = {
-  age: number;
-  nickName: string;
-  // children 은 있어도 없어도 적어주고 옵셔널 적용해주기
-  children?: React.ReactNode;
-};
+import Counter from './components/Counter';
+import NameEditor from './components/NameEditor';
 
-const Sample = ({ age, nickName }: SampleProps) => {
-  return (
-    <div>
-      {age}살이고요, {nickName} 인 샘플입니다.
-    </div>
-  );
-};
-
-const App = () => {
+function App() {
   return (
     <div>
       <h1>App</h1>
-      <Sample age={20} nickName={'홍길동'} />
+      <Counter />
+      <NameEditor />
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/NameEditor.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+type NameEditorProps = {
+  children?: React.ReactNode;
+};
+
+const NameEditor = ({}: NameEditorProps): JSX.Element => {
+  const [name, setName] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setName(e.target.value);
+  };
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      console.log('입력함');
+    }
+  };
+
+  const handleClick = (): void => {
+    console.log('확인');
+    setName('');
+  };
+
+  return (
+    <div>
+      <br />
+      <h2>NameEditor : {name}</h2>
+      <div>
+        <input
+          type="text"
+          value={name}
+          onChange={e => handleChange(e)}
+          onKeyDown={e => handleKeyDown(e)}
+        />
+        <button onClick={handleClick}>확인</button>
+      </div>
+    </div>
+  );
+};
+
+export default NameEditor;
