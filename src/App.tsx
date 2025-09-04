ec6bde62e90cc0127bba1471fb1be70d7087f2ae
maintainline <z.seon.dev@gmail.com>
2025-09-04 13:01:36 +0900

    [docs] auth profile 테이블 적용

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import SignInPage from './pages/SignInPage';
 import TodosPage from './pages/TodosPage';
 import AuthCallback from './pages/AuthCallback';
 import Protected from './components/Protected';
+import ProfilePage from './pages/ProfilePage';
 
 //-------------메뉴 컴포넌트
 const TopBar = () => {
@@ -16,6 +17,7 @@ const TopBar = () => {
       {user && <Link to="/todos">할일</Link>}
       {!user && <Link to="/signup">회원가입</Link>}
       {!user && <Link to="/signin">로그인</Link>}
+      {user && <Link to="/profile">프로필</Link>}
       {user && <button onClick={signOut}>로그아웃</button>}
     </nav>
   );
@@ -38,6 +40,14 @@ function App() {
               element={
                 <Protected>
                   <TodosPage />
+                </Protected>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <Protected>
+                  <ProfilePage />
                 </Protected>
               }
             />
--- src/types/TodoType.ts
+++ src/types/TodoType.ts
@@ -1,8 +1,14 @@
 export type TodoType = { id: string; title: string; completed: boolean };
 
+// 직접 작성 ---------------------------------------
 export type Todo = Database['public']['Tables']['todos']['Row'];
 export type TodoInsert = Database['public']['Tables']['todos']['Insert'];
 export type TodoUpdate = Database['public']['Tables']['todos']['Update'];
+// 사용자 정보
+export type Profile = Database['public']['Tables']['profiles']['Row'];
+export type ProfileInsert = Database['public']['Tables']['profiles']['Insert'];
+export type ProfileUpdate = Database['public']['Tables']['profiles']['Update'];
+// 직접 작성 ---------------------------------------
 
 export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
@@ -14,6 +20,45 @@ export type Database = {
   };
   public: {
     Tables: {
+      memos: {
+        Row: {
+          created_at: string;
+          id: number;
+          memo: string;
+        };
+        Insert: {
+          created_at?: string;
+          id?: number;
+          memo?: string;
+        };
+        Update: {
+          created_at?: string;
+          id?: number;
+          memo?: string;
+        };
+        Relationships: [];
+      };
+      profiles: {
+        Row: {
+          avatar_url: string | null;
+          created_at: string | null;
+          id: string;
+          nickname: string | null;
+        };
+        Insert: {
+          avatar_url?: string | null;
+          created_at?: string | null;
+          id: string;
+          nickname?: string | null;
+        };
+        Update: {
+          avatar_url?: string | null;
+          created_at?: string | null;
+          id?: string;
+          nickname?: string | null;
+        };
+        Relationships: [];
+      };
       todos: {
         Row: {
           completed: boolean;
