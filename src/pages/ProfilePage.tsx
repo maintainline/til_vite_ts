5e78a69683bee05878f845cb6903e34b7c826d6b
maintainline <z.seon.dev@gmail.com>
2025-09-09 13:13:41 +0900

    [docs] file 1장 crud

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState, type ReactEventHandler } from 'react';
 import { useAuth } from '../contexts/AuthContext';
-import { getProfile, updateProfile, uploadAuatar } from '../lib/profile';
+import { getProfile, removeAvatar, updateProfile, uploadAuatar } from '../lib/profile';
 import type { Profile, ProfileUpdate } from '../types/TodoType';
 /**
  * 사용자 프로필 페이지
@@ -81,20 +81,43 @@ function ProfilePage() {
       // 아바타이미지 제거라면
       if (imageRemovalRequest) {
         //storage 에 실제 이미지를 제거함.
+        const success = await removeAvatar(user.id);
+        if (success) {
+          imgUrl = null;
+        } else {
+          alert('이미지 제거에 실패했습니다. 기존이미지가 유지됩니다.');
+        }
       } else if (selectedFile) {
         // 새로운 이미지가 업로드 된다면
         const uploadedImageUrl = await uploadAuatar(selectedFile, user.id);
-        console.log(uploadedImageUrl);
+
+        if (uploadedImageUrl) {
+          // 실제로 업로드 완료후 전달받은 url 문자열을 보관함.
+          //profiles 테이블에 avatar_url 에 넣어줄 문자열
+          imgUrl = uploadedImageUrl;
+        } else {
+          alert('이미지 업로드에 실패했습니다. 닉네임만 저장합니다.');
+        }
       }
 
-      const tempUpdateData: ProfileUpdate = { nickname: nickName };
+      // 실제로 업데이트 진행 부분
+      const tempUpdateData: ProfileUpdate = { nickname: nickName, avatar_url: imgUrl };
+
       const success = await updateProfile(tempUpdateData, user.id);
       if (!success) {
         console.log('프로필 업데이트에 실패하였습니다.');
         return;
       }
-
-      loadProfile();
+      // 업데이트 성공시 초기화 진행
+      setPreviewImage(null);
+      setSelectedFile(null);
+      setImageRemovalRequest(false);
+      setOriginalAvatarUrl(null);
+      if (fileInputRef.current) {
+        fileInputRef.current.value = '';
+      }
+      await loadProfile();
+      alert('프로필이 성공적으로 업데이트 되었습니다.');
     } catch (err) {
       console.log('프로필 업데이트 오류', err);
     } finally {
